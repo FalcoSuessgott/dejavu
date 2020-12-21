105ff5a745bbbe99f9fa9133eda2d4a8af0b445b
Tom Morelly <tom-morelly@gmx.de>
2020-12-21 15:13:44 +0100

    new toc

--- gen-toc.sh
+++ gen-toc.sh
@@ -2,14 +2,14 @@
 
 [ $# -eq 0 ] && echo "no commit message provided." &&  exit 1
 echo "building main ToC"
-gh-md-toc */*.md > README.md 
+gh-md-toc **/*.md > README.md 
 sed '/^[[:space:]]*$/d' README.md
 sed -i '1s/^/# Wiki\n## ToC\n/' README.md
 
-for dir in **/*;do 
+for dir in **/*.md;do 
     file=$dir${dir::-1}.md
     [[ -f $file ]] && gh-md-toc --insert "$file" 1>/dev/null && echo "build ToC for $file"
     rm "$file".* 2>/dev/null 
 done
-# git add . && git commit -m "$(echo $@)" && git push
+git add . && git commit -m "$(echo $@)" && git push
 exit 0 
