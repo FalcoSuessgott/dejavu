24b429fa655456a9cb1c7af2ce8d21f2d8552b80
Tom Morelly <tom-morelly@gmx.de>
2020-12-21 14:50:04 +0100

    syntax

--- gen-toc.sh
+++ gen-toc.sh
@@ -6,7 +6,7 @@ gh-md-toc */*.md > README.md
 sed '/^[[:space:]]*$/d' README.md
 sed -i '1s/^/# Wiki\n## ToC\n/' README.md
 
-for dir in */;do 
+for dir in */*;do 
     file=$dir${dir::-1}.md
     [[ -f $file ]] && gh-md-toc --insert "$file" 1>/dev/null && echo "build ToC for $file"
     rm "$file".* 2>/dev/null 
